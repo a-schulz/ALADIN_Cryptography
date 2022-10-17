f8e2fb985a1105eeaf5352382fe417697af5a125
a-schulz <schulz.alexander2002@gmail.com>
2022-10-17 14:28:36 +0200

    test now with console input

--- src/test/Test.ts
+++ src/test/Test.ts
@@ -1,4 +1,15 @@
 import {ConfigurationRsa} from "../ConfigurationRsa";
 
+const readline = require('readline')
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+rl.question('Enter bitLength:', (bitLength: string) => {
+    new ConfigurationRsa(parseInt(bitLength)).startRsa();
+    rl.close();
+})
+
 //Leichte Aufgabe
-new ConfigurationRsa(2);
