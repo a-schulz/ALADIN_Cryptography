c5637ba9ea0a3c90f52cdbeffe184c785797fc60
a-schulz <schulz.alexander2002@gmail.com>
2022-12-06 18:56:15 +0100

    new custom validation

--- src/frontend/src/components/Config.tsx
+++ src/frontend/src/components/Config.tsx
@@ -1,21 +1,13 @@
-import {useEffect, useState} from "react";
+import {useState} from "react";
 import {RsaConfigHandler} from "../../../backend/RsaConfigHandler";
 import {useNavigate} from 'react-router-dom';
 import {Difficulty} from "../../../backend/Difficulty";
 import {UserConfig} from "../../../backend/UserConfig";
-import {customValidityFromChecks} from "../Utils/customValidityFromChecks";
-import {ShowErrors} from "../Utils/ShowErrors";
-import {validations} from "../Utils/validations";
-import {addCustomValidity, addValidation} from "../Utils/addValidity";
+import {validationConstraints} from "../inputValidation/validationConstraints";
+import {addCustomValidity, addValidationAttributesToElements} from "../inputValidation/addValidation";
 import {useEffectOnce} from "../Utils/useEffectOnce";
 
 //TODO: Beim eingeben der Werte für hard und medium sollten jeweils die Fehler abgefangen und Lösungshilfen angeboten werden
-//TODO .setCustomValidity -> sollte immer den richtigen aktuellen Wert enthalten
-
-export interface ValidationsWithValue {
-    validations: validations,
-    value?:any
-}
 
 export const Config = () => {
     const navigate = useNavigate();
@@ -39,28 +31,11 @@ export const Config = () => {
     /**
      * Containing the validity-checks and the ids of the elements as keys.
      */
-    const validation: Record<string, validations> = {
+    const validationConstraints: Record<string, validationConstraints> = {
         difficulty: {required: true},
-        // bitLength: {required: true}
-        bitLength: {required: true, minlength: {requiredLength: 5}}
+        bitLength: {required: true}
+        // bitLength: {required: true, min: {requiredValue: 3}, max: {requiredValue: 12}}
     };
-
-
-    // TODO: Das wird die neue Strukur sein, dadurch lassen sich die Validierungen komplett automatisieren
-    const validationsComplete: Record<string, ValidationsWithValue> = {
-        // difficulty: {validations: {required: true}, value: 2},
-        difficulty: {validations: {required: true}},
-        // bitLength: {validations: {required: true, minlength: {requiredLength: 5}}, value: 5}
-        bitLength: {validations: {required: true, minlength: {requiredLength: 5}}}
-    }
-
-    const validationMessages = {
-        difficulty: customValidityFromChecks(difficulty, validation.difficulty),
-        bitLength: customValidityFromChecks(bitLength, validation.bitLength)
-    };
-
-    /** Current state of the form.*/
-    const isValidForm: boolean = Object.values(validationMessages).every(x => x === null || x === '');
 
     const renderOption = (text: string) => {
         return <option key={text}>{text}</option>
@@ -68,22 +43,14 @@ export const Config = () => {
 
     const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
-        if (!isValidForm) {
-            document.getElementById(Object.keys(validationMessages).filter((key: string) => (validationMessages[key] != ""))[0]).focus();
-            return;
-        }
         const rsaConfig = new RsaConfigHandler(userConfig).getRSAConfig();
         navigate(difficultyNavigation[userConfig.difficulty], {state: rsaConfig});
     }
 
     useEffectOnce(() => {
-        addValidation(validation);
+        addValidationAttributesToElements(validationConstraints);
+        addCustomValidity(validationConstraints)
     })
-
-    useEffect(() => {
-        addCustomValidity(validationsComplete);
-        }
-    )
 
     return (
         <div className="container">
@@ -94,17 +61,15 @@ export const Config = () => {
                         onChange={(e) => {
                             setDifficulty(Difficulty[e.target.value]);
                         }}
-                        // onInvalid={(e) => e.target.setCustomValidity(validationMessages[e.target.id])}
                 >
                     <option value="">Select Difficulty</option>
                     {options.map(renderOption)}
                 </select>
                 <label htmlFor="bitLength" className="form-label">Enter your bitlength</label>
-                <input type="number" placeholder="3-7" id="bitLength" className="form-control"
+                <input type="text" placeholder="3-7" id="bitLength" className="form-control"
                        onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
                            setBitLength(Number.parseInt(e.target.value));
                        }}
-                       // onInvalid={(e) => e.target.setCustomValidity(validationMessages[e.target.id])}
                 />
                 <button type="submit" className="btn btn-outline-primary">Submit</button>
             </form>
